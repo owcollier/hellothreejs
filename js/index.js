bb3f14b5a4efc7c3a78315e881de6fcecadd5c5c
Owcollier <owcollier@gmail.com>
2018-05-14 13:50:21 -0400

    added init function to contain all set up logic and variable assignment

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene, camera, renderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+let geometry, material;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let cube;
 
-camera.position.z = 5;
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+}
 
 function animate() {
   requestAnimationFrame( animate );
@@ -20,4 +28,6 @@ function animate() {
   cube.rotation.y += 0.005;
   renderer.render( scene, camera );
 }
+
+init();
 animate();
